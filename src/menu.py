3b618754929b77c9d1b9bcae97c200fe74271aff
Maxime <maxime.dharboulle@gmail.com>
2022-01-04 00:35:21 +0100

    change background

--- src/menu.py
+++ src/menu.py
@@ -12,29 +12,22 @@ class Menu(arcade.View):
         background_img_path = "assets/images/menu_wallpaper.jpg"
         self.background = arcade.load_texture(background_img_path)
 
-        self.display_timer = 0
-        self.show_instructions = False
-    
-    def on_update(self, delta_time: float) -> None:
-        self.display_timer -= delta_time
-        if self.display_timer <= 0:
-            self.show_instructions = not self.show_instructions
-            self.display_timer = 0.5
+        arcade.set_background_color(arcade.color.MIDNIGHT_GREEN)
     
     def on_draw(self) -> None:
         arcade.start_render()
 
-        arcade.draw_lrwh_rectangle_textured(
-            0,0,
-            self.window.width, self.window.height,
-            self.background,
-        )
+        # arcade.draw_lrwh_rectangle_textured(
+        #     0,0,
+        #     self.window.width, self.window.height,
+        #     self.background,
+        # )
 
         arcade.draw_text(
             "Main menu",
             start_x=self.window.width // 2 - 350,
             start_y=self.window.height // 2 + self.window.height // 4,
-            color=arcade.color.DARK_BLUE_GRAY,
+            color=arcade.color.DARK_CYAN,
             font_size=80,
             bold=True
         )
@@ -44,7 +37,7 @@ class Menu(arcade.View):
             center_y=self.window.height // 3 + 20,
             width=self.window.width // 1.4,
             height=self.window.height // 3,
-            color=(200,200,200,150),
+            color=(200,200,200,20),
         )
     
         arcade.draw_text(
@@ -56,23 +49,22 @@ class Menu(arcade.View):
             bold=True
         )
 
-        if self.show_instructions:
-            arcade.draw_text(
-                "Press M for manual mode",
-                start_x=self.window.width // 2 - 375,
-                start_y=self.window.height // 3 + 50,
-                color=arcade.color.DARK_BLUE_GRAY,
-                font_size=40,
-                bold=True
-            )
-            arcade.draw_text(
-                "Press A for IA mode",
-                start_x=self.window.width // 2 - 300,
-                start_y=self.window.height // 4,
-                color=arcade.color.DARK_BLUE_GRAY,
-                font_size=40,
-                bold=True
-            )
+        arcade.draw_text(
+            "Press M for manual mode",
+            start_x=self.window.width // 2 - 375,
+            start_y=self.window.height // 3 + 50,
+            color=arcade.color.DARK_CYAN,
+            font_size=40,
+            bold=True
+        )
+        arcade.draw_text(
+            "Press A for IA mode",
+            start_x=self.window.width // 2 - 300,
+            start_y=self.window.height // 4,
+            color=arcade.color.DARK_CYAN,
+            font_size=40,
+            bold=True
+        )
             
         
     
